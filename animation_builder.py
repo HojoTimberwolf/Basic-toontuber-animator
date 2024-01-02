90128d72b407243a572b2b4d011aa48405565195
Hojo <Hojotimberhk@gmail.com>
2024-01-03 00:32:36 +0800

    parse functions into in appropriate classes, cleaning up code

--- animation_builder.py
+++ animation_builder.py
@@ -16,15 +16,18 @@ import os
 import numpy as np
 
 class animation_framer():
-    def __init__(self,img_size=(1583,1776), bkgd_color = (0,204,0)):
+    def __init__(self,img_size=(1583,1776), bkgd_color = (0,204,0),keyframe_loc = None):
         # variables
-        self.keyframe_loc = {
-            "tail":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Additional","file_names":"FX_Tail.png"},
-            "body":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Poses","file_names":"FX_pose_{stance}.png"},
-            "eyes":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Additional","file_names":"FX_eyes.png"},
-            "VR":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Additional","file_names":"FX_VR.png"},
-            "VR_eyes":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Additional","file_names":"FX_VR_eyes.png"}
-            }
+        if keyframe_loc is None:            
+            self.keyframe_loc = {
+                "tail":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Additional","file_names":"FX_Tail.png"},
+                "body":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Poses","file_names":"FX_pose_{stance}.png"},
+                "eyes":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Additional","file_names":"FX_eyes.png"},
+                "VR":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Additional","file_names":"FX_VR.png"},
+                "VR_eyes":{"length":1,"directory":r".\animation_builder\Key_frames\{emotion}\Additional","file_names":"FX_VR_eyes.png"}
+                }
+        else:
+            self.keyframe_loc = keyframe_loc
 
         self.img_size = img_size
         self.bkgd_color = bkgd_color    
